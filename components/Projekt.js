77a80cbc92e9de3043f56527f28aecc92e3b8554
Mark Julien Hahn <mail@markjulienhahn.de>
2022-10-11 19:07:57 +0200

    ...

--- components/Projekt.js
+++ components/Projekt.js
@@ -55,7 +55,7 @@ const Projekt = ({
       {projekt.videolink ? (
         <div className="embed-container">
           <iframe
-            src={`https://vimeo.com/${projekt.videolink}`}
+            src={`https://player.vimeo.com/video/${projekt.videolink}`}
             width="640"
             height="360"
             frameBorder="0"
