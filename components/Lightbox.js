dab2898ad7bbff19464b4254e600fa3b8381d873
Mark Julien Hahn <mail@markjulienhahn.de>
2022-10-18 12:15:31 +0200

    ...

--- components/Lightbox.js
+++ components/Lightbox.js
@@ -5,14 +5,15 @@ const Lightbox = ({ image, setImage }) => {
     <div className="lightboxWrapper">
       {image ? (
         <div
-          className="lightboxInner"
           onClick={() => setImage(null)}
-          style={{ height: "100%", position: "relative" }}
+          style={{ position: "fixed", top: "0",height: "100vw", position: "relative", minWidth: "50vw" }}
         >
           <Image
+            className="lightboxInner"
             src={image.foto.url}
             width={image.foto.metadata?.width}
             height={image.foto.metadata?.height}
+            layout="responsive"
           />
         </div>
       ) : (
